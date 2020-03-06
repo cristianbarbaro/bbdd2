0eb1d6bc4b9fd033cb914f3b2a3e877dc3cac6a9
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2020-03-06 17:08:18 -0300

    cuando se borra una granja, se borran todas sus dependencias

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,5 +1,5 @@
 class Farm < ApplicationRecord
-    has_many :plots, :dependent => :restrict_with_error
+    has_many :plots, :dependent => :destroy
     has_and_belongs_to_many :users
     validates :name, :description, presence: true
     validates :name, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
